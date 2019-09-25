0347e3d301576fd782376e72915a6eb09f7f8bd1
19leunam93 <tech.spuur@quickline.ch>
2019-09-25 10:39:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@ window.onload = function() {
 
 	//-- Initialisation ---------
 
-	// Message-Box //
-	////////////////
+	//-- Message-Box ------
 	// clear message box
 	const clearMessages = function() {
 		let $tmpl = html``;
@@ -36,9 +35,8 @@ window.onload = function() {
 	}
 	window.setMessage = setMessage;
 
-	// Router //
-	////////////
 
+	//-- Router ----------
 	// eventlistener on internal links to modify the url
 	const indexLinks = function() {
 		let activeRoutes = Array.from(document.querySelectorAll('[route]'));
@@ -58,20 +56,6 @@ window.onload = function() {
 	};
 	const myRouter = new Router('myRouter', siteRoutes)
 
-	// Global relevant HTML-Elements //
-	///////////////////////////////////
-
-	const appContent = document.getElementById('app-content');
-	const siteTitle = document.getElementById('site-titel');
-
-	// Authorisation //
-	//////////////////
-	const auth = new Authentication();
-	window.auth = auth;
-
-
-	//---------------------------
-
 	const internalLinks = function(event) {
 		var route = event.target.getAttribute('route');
 		window.history.pushState({}, '', route);
@@ -83,7 +67,21 @@ window.onload = function() {
 		modifyContent();
 	};
 
-	// modify HTML-content based on route
+
+	//-- HTML-Elements ----
+	const appContent = document.getElementById('app-content');
+	const siteTitle = document.getElementById('site-titel');
+
+
+	//-- Authorisation ----
+	const auth = new Authentication();
+	window.auth = auth;
+
+	//--end Initialisation-------
+
+
+
+	// modify app-content Container based on route
 	const modifyContent = function() {
 		clearMessages();
 		let currentPath = window.location.pathname;		
@@ -105,8 +103,6 @@ window.onload = function() {
 			let viewName = routeInfo.view + 'View';
 			let view = dynView(viewName, routeInfo);
 			window.view = view;
-			// authentification of this view (Are you authorized to see this view?)
-			//auth.doAuthorisation(viewName);
 
 			// insert content into <div id="app-content">
 			view.renderView(appContent);
